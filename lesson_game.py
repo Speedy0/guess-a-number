e0970446a9e0bf2f79959f0c87108f77afdec7bd
Speedy <origino@bk.ru>
2018-11-06 02:42:41 +0300

    Update lesson_game.py

--- lesson_game.py
+++ lesson_game.py
@@ -1,9 +1,11 @@
 # The task, read a game.
 # Игра "угадай число" (проще)
+# ValueError
+
 
 import random
 
-python = random.randint(1, 20)
+python = random.randint(1, 30)
 
 
 print('System loading...')
@@ -12,9 +14,9 @@ print('If you win, then you will survive.')
 
 print('''Rules: 
     I guessing a number, if you have guessed, then you won! Otherwise, you dead!
-    You will have 3 lives ''')
+    You will have 4 lives ''')
 
-health = 3
+health = 4
 
 while health > 0:
     user_input = int(input('Enter a number. I recommend to think({} attempts): '.format(health)))
@@ -25,10 +27,14 @@ while health > 0:
     elif user_input > python:
         print('System a number - Less')
         health -= 1
+        if health == 0:
+            print('You lose! You are out of attempts. Number was - ', python)
+            break
         continue
     elif user_input < python:
         print('System a number - More')
         health -= 1
+        if health == 0:
+            print('You lose! You are out of attempts. Numbers was - ', python)
+            break
         continue
-    else:
-        print('You lose! You are out of attempts.')
